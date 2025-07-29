0398036129f6898b453979b170cc3165e25c173c
User A <userA@example.com>
2025-07-29 18:23:41 +0530

    this si the commit till the startup cadr is made for the 3 links where the title of the project is displayed

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,4 +1,23 @@
-export default function Home() {
+import SearchForm from "@/components/SearchForm";
+import StartupCard from "@/components/StartupCard";
+export default async function Home({searchParams}:{
+  searchParams:Promise<{query?:string}>
+}) {
+  const query=(await searchParams).query;
+  const posts = [
+  {
+    _createdAt: new Date(),
+    views: 55,
+    author: { _id: 1,name:'Sanskar' },
+    _id: 1,
+    description: "This is a description",
+    image:
+      "https://images.unsplash.com/photo-1634912314704-c646c586b131?q=80&w=2940&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    category: "Robots",
+    title: "We Robots",
+  },
+];
+
   return (
     <>
       <section className="pink_container">
@@ -11,7 +30,24 @@ export default function Home() {
           Submit Ideas, Vote on Pitches, and Get Noticed in Virtual Competitions.
         </p>
 
+
+      <SearchForm query={query}/>
+      
       </section>
+      <section className="section_container">
+        <p className="text-30-semibold">
+          {query ?`Search results for "${query}"` : 'All Startups'}
+        </p>
+        <ul className="mt-7 card_grid">
+          {posts?.length > 0 ? (
+              posts.map((post: StartupCardType) => (
+              <StartupCard key={post?._id} post={post}/>
+  ))
+) : (
+          <p className="no-results">No startups found</p>
+)}  
+        </ul>
+        </section>
     </>
   );
 }
