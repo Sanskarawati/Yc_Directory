c06e3b2e2d2ecfb85ee2b595c8e28d749d45ca26
User A <userA@example.com>
2025-07-30 17:35:29 +0530

    card section completed successfully

--- components/StartupCard.tsx
+++ components/StartupCard.tsx
@@ -2,8 +2,10 @@ import React from 'react'
 import { formatDate } from '@/lib/utils'
 import { EyeIcon } from 'lucide-react'
 import Link from 'next/link'
+import Image from "next/image";
+import { Button } from "@/components/ui/button";
 const StartupCard = ({post}:{post:StartupTypeCard}) => {
-    const{_createdAt,views,author:{_id:authorId,name},title,category,_id,image}=post;
+    const{_createdAt,views,author:{_id:authorId,name},title,category,_id,image,description}=post;
   return (
     <li className="startup-card group">
         <div className="flex-between">
@@ -25,10 +27,31 @@ const StartupCard = ({post}:{post:StartupTypeCard}) => {
                     <Link href={`/startup/${_id}`}>
                     <h3 className="text-26-semibold line-clamp-1">{title}</h3>
                     </Link>
+                    
                 </div>
-
+                <Link href={`/user/${authorId}`}>
+                    <Image src="https://placehold.co/600x400" alt="image" width={48} height={48} className="rounded-full"/>
+                    </Link>
             
         </div>
+        <Link href={`/startup/${_id}`}>
+                    <p className="startup-card_desc">{description}
+                        
+
+                    </p>
+                    <img src={image} alt="placeholder" className="startup-card_img"/>
+                    </Link>
+
+                    <div className="flex-between gap-3 mt-5">
+                        <Link href={`/?query=${category.toLowerCase()}`}>
+                        <p className="text-16-medium">{category}</p></Link>
+                        <Button className="startup-card_btn" asChild >
+                            <Link href={`/startup/${_id}`}>
+                            Details
+                            </Link>
+                        </Button>
+                    </div>
+                    
       
     </li>
   )
